6237159420000de66186d5edb0c623fba9fa9b2f
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-10-05 15:06:00 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Copyright (C) 2024 BlazeSnow
+﻿//Copyright (C) 2024 BlazeSnow
 //保留所有权利
 //本程序以GNU General Public License v3.0的条款发布
 #include <iostream>
@@ -69,7 +69,7 @@ static int parenthesis2(int num[], char cul[]) {
 }
 
 int main() {
-	system("chcp 65001");
+	system("chcp 54936");
 	cout << "Copyright (C) 2024 BlazeSnow.保留所有权利。" << endl;
 	cout << "本程序以GNU General Public License v3.0的条款发布。" << endl;
 	cout << "当前程序版本号：v1.2.4" << endl;
